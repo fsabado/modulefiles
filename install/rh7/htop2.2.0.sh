044fbdee3b8540b75354c5b3474a1f456adbdf4b
Francis Sabado <fs8080@gmail.com>
2020-06-04 01:47:40 -0600

    Added htop modules and install script

---
+++ install/rh7/htop2.2.0.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Install htop2.2.0
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && source ${DIR}/common.sh
+
+PACKAGE_LIST=(
+    https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/h/htop-2.2.0-3.el7.x86_64.rpm
+)
+
+INSTALL_DIR=htop2.2.0
+installRPMS "${INSTALL_DIR}" "${PACKAGE_LIST[@]}"
+
+
+
